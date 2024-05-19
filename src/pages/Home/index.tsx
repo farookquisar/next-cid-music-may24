2ef557f6bdaafcf2ae59b20df732afda861ac6eb
farookquisar <farook.quisar@gmail.com>
2024-05-19 08:19:41 +0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,9 @@ import banner from "../../assets/images/banner2.png";
 import RenderRows from "../../components/RenderRows";
 import { firstRow, secondRow } from "../../data/songs";
 
-const Home = () => {
+const Home = () =>
+{
+	// main
 	return (
 		<div className="text-white md:pl-[370px] pl-5 px-5 py-5 flex flex-col w-full gap-5">
 			<div className="w-full rounded-xl md:h-[calc(100vh-140px)] h-auto flex flex-col items-center gap-5 bg-[#121212] px-5 md:py-5 pb-20 pt-5">
